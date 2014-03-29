c36ce25c12d9dcaec25639aacd97ba9f45ca55cf
John Morgan <johnmorgan@ebays-MacBook-Air.local>
2014-03-29 17:23:48 -0600

    Return users friends data components
    Added the components that connect to and return the users friends
    location, music and more as a user object

--- f-recommend/src/calculate/calculate.java
+++ f-recommend/src/calculate/calculate.java
@@ -10,8 +10,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import facebook4j.Facebook;
+import facebook4j.FacebookException;
 import facebook4j.Friend;
+import facebook4j.Reading;
 import facebook4j.ResponseList;
+import facebook4j.User;
+import facebook4j.internal.org.json.JSONArray;
+import facebook4j.internal.org.json.JSONException;
+import facebook4j.internal.org.json.JSONObject;
 
 /**
  * Servlet implementation class calculate
@@ -32,9 +38,56 @@ public class calculate extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("yay\n");
 		Facebook facebook = (Facebook)request.getSession().getAttribute("facebook");
-		System.out.println("yay\n");
+		try {
+			ResponseList<Friend> list = facebook.getFriends();
+		} catch (FacebookException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Multiple FQL
+		Map<String, String> queries = new HashMap<String, String>();
+		//queries.put("all friends", "SELECT uid2 FROM friend WHERE uid1=me()");
+		queries.put("my name", "SELECT name,music FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me() LIMIT 500)");
+		Map<String, JSONArray> result = null;
+		
+		
+		try {
+			result = facebook.executeMultiFQL(queries);
+		} catch (FacebookException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*
+		JSONArray allFriendsJSONArray = result.get("all friends");
+		for (int i = 0; i < allFriendsJSONArray.length(); i++) {
+		    JSONObject jsonObject = null;
+			
+		    try {
+				jsonObject = allFriendsJSONArray.getJSONObject(i);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 // Getting user's email address only
+			try {
+				User user = facebook.getUser((String) jsonObject.get("uid2"));
+				System.out.println(user);
+			} catch (FacebookException | JSONException e1) {
+				// TODO Auto-geneSystem.out.println(jsonObject);rated catch block
+				e1.printStackTrace();
+			}
+			
+		}*/
+		JSONArray myNameJSONArray = result.get("my name");
+		try {
+			System.out.println(myNameJSONArray.getJSONObject(0).get("name"));
+			System.out.println(myNameJSONArray.getJSONObject(0).get("music"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		// Map
 		HashMap<String, Integer> h = new HashMap<String, Integer>();
